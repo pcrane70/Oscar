b815378690db66ff44c5ddac66c244673b336003
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-06-25 09:54:34 -0400

    New

--- src/com/comcast/oscar/examples/REST.java
+++ src/com/comcast/oscar/examples/REST.java
@@ -1,10 +1,10 @@
 package com.comcast.oscar.examples;
 
+import static spark.Spark.*;
 
 public class REST {
 
 	public static void main(String[] args) {
-
 		get("/hello", (req, res) -> "Hello World");
 	}
 
