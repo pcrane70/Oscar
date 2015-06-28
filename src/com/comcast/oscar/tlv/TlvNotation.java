83cdfbf2579c1a77d7b5e9e95c9469f373f24b5f
Maurice Garcia <maurice.garcia.2015@gmail.com>
2015-06-27 23:09:35 -0400

    Commit New Code

--- src/com/comcast/oscar/tlv/TlvNotation.java
+++ src/com/comcast/oscar/tlv/TlvNotation.java
@@ -1,27 +1,42 @@
 package com.comcast.oscar.tlv;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.comcast.oscar.utilities.HexString;
 
 public class TlvNotation {
 
 	private String sTlvNotation = "";
+	private HexString hsTlvNotation = null;;
 	
+
 	/**
 	 * 
 	 * @param sTlvNotation 	Acceptable Inputs 24.1.2.3 , 3
 	 * 						Must be Positive Integers 0 or greater
-	 */
-	public TlvNotation(String sTlvNotation) {
+	 * @throws TlvException*/
+	public TlvNotation(String sTlvNotation) throws TlvException {
 		this.sTlvNotation = sTlvNotation;
+
+		this.hsTlvNotation = new HexString();
+		
+		for (String sType : Arrays.asList(sTlvNotation.split("\\."))) {
+			
+			if (sType.matches("-?\\d+")) {
+				this.hsTlvNotation.add(Integer.parseInt(sType));
+			} else {
+				this.hsTlvNotation = new HexString();
+				this.sTlvNotation = "";
+				throw new TlvException("TlvNotation() Constructor Argument is not an Integer: " + sType);
+			}
+					
+		}
+		
 	}
 	
-	/**
-	 * @param sTlvNotation Example 24.1.3 of 3
-	 * @return True = Valid | False != Valid*/
-	public static boolean isTlvNotation(String sTlvNotation) {
-		return false;
-	}
-	
+
 	/**
 	 **/
 	public String toString() {
@@ -32,14 +47,14 @@ public class TlvNotation {
 	 * 
 	 * @return Byte representation of TLVNotation*/
 	public byte[] toByteArray() {
-		return null;
+		return hsTlvNotation.toByteArray();
 	}
 	
 	/**
 	 * 
 	 * @return Number of bytes of TLV Notation*/
 	public int size() {
-		return 0;
+		return hsTlvNotation.toByteArray().length;
 	}
 	
 	/**
@@ -47,7 +62,7 @@ public class TlvNotation {
 	 * @param iIndex Example 24.2.4.6.8 -> Index=2 Return -> 4
 	 * @return Integer of the Type specified in Index*/
 	public int getType(int iIndex) {
-		return 0;
+		return hsTlvNotation.toByteArray()[iIndex];
 	}
 	
 	/**
